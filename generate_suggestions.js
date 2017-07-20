e6be95272febfe9e55745b91a4e4cde49801930c
Profir-Petru Partachi <profir.p.partachi@gmail.com>
2017-07-20 16:13:58 +0100

    First sketch of the chrome plugin to generate suggestions. Will use a headless python backend that has to be started separately.

---
+++ generate_suggestions.js
@@ -0,0 +1,111 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+        // chrome.tabs.query invokes the callback with a list of tabs that match the
+        // query. When the popup is opened, there is certainly a window and at least
+        // one tab, so we can safely assume that |tabs| is a non-empty array.
+        // A window can only have one active tab at a time, so the array consists of
+        // exactly one tab.
+        var tab = tabs[0];
+
+        // A tab is a plain object that provides information about the tab.
+        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        var url = tab.url;
+
+        // tab.url is only available if the "activeTab" permission is declared.
+        // If you want to see the URL of other tabs (e.g. after removing active:true
+        // from |queryInfo|), then the "tabs" permission is required to see their
+        // "url" properties.
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+
+        callback(url);
+    });
+
+    // Most methods of the Chrome extension APIs are asynchronous. This means that
+    // you CANNOT do something like this:
+    //
+    // var url;
+    // chrome.tabs.query(queryInfo, function(tabs) {
+    //   url = tabs[0].url;
+    // });
+    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+var port = null;
+var JIRA_RE = /.*\/jira\/.*\/([a-zA-Z\-0-9]+) \1/;
+var GitHub_RE = /https?:\/\/github\.com\/(.*)\/issues\/([0-9]+) \1 \2/;
+
+var getKeys = function(obj){
+    var keys = [];
+    for(var key in obj){
+        keys.push(key);
+    }
+    return keys;
+};
+
+function displayMessage(text) {
+    document.getElementById('response').innerHTML = "<p>" + text + "</p>";
+}
+
+function updateUiState() {
+    if (port) {
+        document.getElementById('connect-button').style.display = 'none';
+        document.getElementById('send-message-button').style.display = 'block';
+    } else {
+        document.getElementById('connect-button').style.display = 'block';
+        document.getElementById('send-message-button').style.display = 'none';
+    }
+}
+
+function sendNativeMessage() {
+    var repo_issue = null;
+    getCurrentTabUrl(function(url) {
+        repo_issue = url.replace(GitHub_RE, '$1 $2').split(' ');
+    });
+    var repo = repo_issue[0];
+    var issue = repo_issue[1];
+    var message = {"Repository": repo, "Issue": issue};
+    port.postMessage(message);
+}
+
+function onNativeMessage(message) {
+    displayMessage('<ul><li>' + message.items.join('</li><li>') + '</li></ul>');
+}
+
+function onDisconnected() {
+    displayMessage("Failed to connect: " + chrome.runtime.lastError.message);
+    port = null;
+    updateUiState();
+}
+
+function connect() {
+    var hostName = "tlinker";
+    port = chrome.runtime.connectNative(hostName);
+    port.onMessage.addListener(onNativeMessage);
+    port.onDisconnect.addListener(onDisconnected);
+    updateUiState();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('connect-button').addEventListener(
+        'click', connect);
+    document.getElementById('send-message-button').addEventListener(
+        'click', sendNativeMessage);
+    updateUiState();
+});
